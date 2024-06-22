d43270db7309d5e149a1f04e3df9eb6db253791e
ErickMarroquin <erickalejandro0112@gmail.com>
2024-06-22 00:00:34 -0600

    comentario2

--- src/Main.java
+++ src/Main.java
@@ -47,3 +47,58 @@ class NumeroMayor {
         return mayor;
     }
 }
+
+import java.util.Scanner;
+
+public class MenuEjercicios {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+
+        while (opcion != 4) {
+            System.out.println("Menu de Ejercicios");
+            System.out.println("1. Ejercicio 1");
+            System.out.println("2. Ejercicio 2");
+            System.out.println("3. Ejercicio 3");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opcion: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    ejercicio1();
+                    break;
+                case 2:
+                    ejercicio2();
+                    break;
+                case 3:
+                    ejercicio3();
+                    break;
+                case 4:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida, intente de nuevo.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void ejercicio1() {
+        System.out.println("Ejecutando Ejercicio 1...");
+        // Logica del Ejercicio 1
+    }
+
+    public static void ejercicio2() {
+        System.out.println("Ejecutando Ejercicio 2...");
+        // Logica del Ejercicio 2
+    }
+
+    public static void ejercicio3() {
+        System.out.println("Ejecutando Ejercicio 3...");
+        // Logica del Ejercicio 3
+    }
+}
+
